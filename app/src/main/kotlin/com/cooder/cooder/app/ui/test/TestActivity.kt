ae5498ff177e923e0707430dce53eed49587daa4
cooder <2965502228@qq.com>
2023-02-26 03:48:07 +0800

    2.0.6 - 20230226[v1] - 更新图标

--- app/src/main/kotlin/com/cooder/cooder/app/ui/test/TestActivity.kt
+++ app/src/main/kotlin/com/cooder/cooder/app/ui/test/TestActivity.kt
@@ -1,6 +1,8 @@
 package com.cooder.cooder.app.ui.test
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Process
 import androidx.appcompat.app.AppCompatActivity
 import com.cooder.cooder.app.databinding.ActivityTestBinding
 import com.cooder.cooder.library.util.expends.immersiveStatusBar
@@ -23,5 +25,13 @@ class TestActivity : AppCompatActivity() {
 		binding = ActivityTestBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 		immersiveStatusBar(false)
+		
+		binding.btn.setOnClickListener {
+			val intent = packageManager.getLaunchIntentForPackage(packageName)
+			intent?.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+			startActivity(intent)
+			// 把自己干掉
+			Process.killProcess(Process.myPid())
+		}
 	}
 }
